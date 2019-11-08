c280122b8bf5c53f5270707badb5b9bd45afd1bb
foushware <lionmoon60@yahoo.com>
2019-11-08 22:38:28 +0200

    update and delete an item backend

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -11,15 +11,32 @@ const Mutations = {
     console.log(item);
     return item;
 
-  }
+  },
+  updateItem(parent,args,ctx,info){
+    //first take a copy of the updates
+    const updates= {...args};
+    //remove the ID from the updates
+    delete updates.id;
+    //run the update method
+    return ctx.db.mutation.updateItem({
+      data:updates,
+      where:{
+        id:args.id,
+      },
+    },
+    info  // info make the function know what to return
 
-  // createDog(parent,args,ctx,info){
-  //   global.dogs=global.dogs || [];
-  //   const newDog={name:args.name};
-  //   console.log(args);
-  //   global.dogs.push(newDog);
-  //   return newDog;
-  // }
+    )
+  },
+  async deleteItem(parent,args,ctx,info){
+    const where={id:args.id};
+    //1.find the item
+    const item = await ctx.db.query.item({where},`{id title}`);
+    //2.check if they own that item ,or have a permission
+    //TODO
+    //3.delete it !
+    return ctx.db.mutation.deleteItem({where},info);
+  }
 };
 
 module.exports = Mutations;
