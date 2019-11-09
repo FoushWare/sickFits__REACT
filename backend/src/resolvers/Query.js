0d9f0ae1be607a53cfeb89eb7b9924a3576b8fc9
foushware <lionmoon60@yahoo.com>
2019-11-09 14:57:01 +0200

    pagination backend

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,6 +2,7 @@ const {forwardTo}=require('prisma-binding');
 const Query = {
   items:forwardTo('db'),
   item:forwardTo('db'),
+  itemsConnection:forwardTo('db')
 // if query in yoga the same as in prisma you can use forwardto
   // async items(parent,args,ctx,info){
   //   const items= await ctx.db.query.items();
