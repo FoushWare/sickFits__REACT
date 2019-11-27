24a65608faa73d8ce86630992876d285506eb9cf
foushware <lionmoon60@yahoo.com>
2019-11-28 00:42:01 +0200

    change the port in frontent

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
+export const prodendpoint = `https://sickfits-yoga-productionfoush.herokuapp.com/`;
 export const perPage = 4;
