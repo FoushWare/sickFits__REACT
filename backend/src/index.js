69351899b937bca472177226a7e85f6cb802d909
foushware <mr.fouad992@gmail.com>
2021-04-01 19:35:55 +0200

    no-corsss2

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,13 +36,13 @@ server.express.use(async (req, res, next) => {
 });
 // start it
 server.start(
-  // {
-  //   cors: {
-  //     credentials: true,
-  //     // this port with frontend will only be the only user of this server
-  //     origin: [process.env.FRONTEND_URL],
-  //   },
-  // }, 
+  {
+    cors: {
+      credentials: true,
+      // this port with frontend will only be the only user of this server
+      origin: false,
+    },
+  },
   deets => {
     console.log(`Server is now running on port
     http:/localhost:${deets.port}`);
