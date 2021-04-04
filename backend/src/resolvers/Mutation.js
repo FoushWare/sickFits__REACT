1566866c579db16bdde376db18264df9616de3c4
foushware <mr.fouad992@gmail.com>
2021-04-04 23:02:00 +0200

    check token and response1

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -109,7 +109,7 @@ const Mutations = {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 365,
     });
-    console.log("ctx response" + ctx.response);
+    console.log("ctx response" + JSON.stringify(ctx.response));
     // 5. Return the user
     return user;
   },
