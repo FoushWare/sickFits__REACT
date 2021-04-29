0d450525a849e89447dd97acc5945349533c0b21
foushware <mr.fouad992@gmail.com>
2021-04-29 02:31:03 +0200

    frontend order time formatDistance not accept string as input

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Query } from 'react-apollo';
-import { formatDistance } from 'date-fns';
+import { formatDistance, parseISO } from 'date-fns';
 import Link from 'next/link';
 import styled from 'styled-components';
 import gql from 'graphql-tag';
