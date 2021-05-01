6710a1180085101c8b74edb5d5ce1c16b66db31f
foushware <mr.fouad992@gmail.com>
2021-05-01 10:21:27 +0200

    💎 02-config keystone with mongoose adapter

---
+++ backend/keystone.ts
@@ -0,0 +1,44 @@
+import 'dotenv/config'; // Get all .env variables
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const { MongooseAdapter } = require('@keystonejs/adapter-mongoose');
+
+// define the DB
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits';
+
+
+
+
+// session config for authentication
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How Long they stay signed in ?
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    adapter:'mongoose',
+    url:databaseURL,
+    // TODO: Add data seeding here
+  }, // Keystone deal with entity[users,products,orders] as lists
+  lists: createSchema({
+    // Schema items go in here`:w
+  }),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: Add session values here
+});
