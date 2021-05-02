a6efed42461683acf7f3541a2df7cd2cb61e2f1c
foushware <mr.fouad992@gmail.com>
2021-05-02 04:56:16 +0200

    🦊 04- Add session to allow specific users to access admin UI

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,11 @@
 import 'dotenv/config'; // Get all .env variables
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { createAuth } from '@keystone-next/auth';
+
 import { User } from './schemas/User';
 
 // define the DB
@@ -13,29 +19,49 @@ const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // How Long they stay signed in ?
   secret: process.env.COOKIE_SECRET,
 };
-
-export default config({
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+// Auth to access the Admin UI
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO : add in init roles here
   },
-  db: {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: Add data seeding here
-  }, // Keystone deal with entity[users,products,orders] as lists
-  lists: createSchema({
-    // Schema items go in here`:w
-    User,
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: Add session values here
 });
+
+export default withAuth(
+  config({
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+    }, // Keystone deal with entity[users,products,orders] as lists
+    lists: createSchema({
+      // Schema items go in here`:w
+      User,
+    }),
+    ui: {
+      // TODO: change this for roles
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        return !!session?.data;
+      },
+    },
+    // TODO: Add session values here
+    session: withItemData(statelessSessions(sessionConfig), {
+      // Graphql Query
+      User: 'id name email',
+    }),
+  })
+);
