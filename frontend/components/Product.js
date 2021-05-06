eb1dcb0c0a0a1f5eaabb3d26e8deb58a5e644883
foushware <mr.fouad992@gmail.com>
2021-05-06 13:34:40 +0200

    🐸 13- Update the product

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -6,19 +6,28 @@ import formatMoney from '../lib/formatMoney';
 
 export default function Product({ product }) {
   return (
-    <>
-      <ItemStyles>
-        <img
-          src={product?.photo?.image?.publicUrlTransformed}
-          alt={product.name}
-        />
-        <Title>
-          <Link href={`/product/${product.id}`}>{product.name}</Link>
-        </Title>
-        <PriceTag>{formatMoney(product.price)}</PriceTag>
-        <p>{product.description}</p>
-        {/* TODO: Add buttons to edit and delte item */}
-      </ItemStyles>
-    </>
+    <ItemStyles>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed}
+        alt={product.name}
+      />
+      <Title>
+        <Link href={`/product/${product.id}`}>{product.name}</Link>
+      </Title>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <p>{product.description}</p>
+      <div className="buttonList">
+        <Link
+          href={{
+            pathname: 'update',
+            query: {
+              id: product.id,
+            },
+          }}
+        >
+          Edit ✏️
+        </Link>
+      </div>
+    </ItemStyles>
   );
 }
---
+++ frontend/components/UpdateProduct.js
@@ -0,0 +1,107 @@
+import { useMutation, useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
+import Form from './styles/Form';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+const UPDATE_PRODUCT_MUTATION = gql`
+  mutation UPDATE_PRODUCT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateProduct(
+      id: $id
+      data: { name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateProduct({ id }) {
+  // 1- first fetch the product
+  const { loading, error, data } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: {
+      id,
+    },
+  });
+
+  // 2- update the formHook
+  const { inputs, handleChange, clearForm, resetForm } = useForm(data?.Product);
+
+  // 3- Handle the updates
+  const [
+    updateProduct,
+    { loading: updateLoading, error: updateError, data: updateData },
+  ] = useMutation(UPDATE_PRODUCT_MUTATION);
+
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const res = await updateProduct({
+          variables: {
+            id,
+            name: inputs.name,
+            description: inputs.description,
+            price: inputs.price,
+          },
+        }).catch(console.error);
+      }}
+    >
+      <DisplayError error={error || updateError} />
+      <fieldset disabled={updateLoading} aria-busy={updateLoading}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Update Product</button>
+      </fieldset>
+    </Form>
+  );
+}
